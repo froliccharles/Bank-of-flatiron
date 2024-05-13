a10a20cd867e21f28ba144ec51c678899b395ed7
charlesmumo <charles.mumo@student.moringaschool.com>
2024-05-13 18:44:54 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import SearchBar from './Components/SearchBar';
+import Table from './Components/TransactionTable';
+import Form from './Components/TransactionForm';
+
 
 function App() {
+  const [transactions, setTransactions] = useState([]);
+  const addTransaction = (transaction) => {
+    setTransactions([...transactions, transaction]);
+  };
+
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container"> {/* Container to span the whole width of the page */}
+      <h1 className="App">The Royal Bank of Flatiron</h1>
+      <SearchBar value={searchTerm} onChange={(value) => setSearchTerm(value)} /> {/* Pass onChange prop */}
+      <Form addTransaction={addTransaction} />
+      
     </div>
   );
 }
 
 export default App;
+
+
